2078a822ff3ab988a03d3aa9d6a0f32d5538585a
infomaligoioceane-dotcom <info.maligoi.oceane@gmail.com>
2026-03-20 11:26:37 +0100

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/logo.png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <meta name="description" content="Océane Maligoi - Agence Stratégique Mode. Accompagnement stratégique pour marques de luxe premium & distributeurs." />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
